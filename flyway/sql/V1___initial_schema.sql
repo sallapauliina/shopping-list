6ee2cb27e9912e39f215e695412e73c473261a7c
sallapauliina <salla.p.lyytikainen@aalto.fi>
2025-01-16 12:44:37 +0200

    final

--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE shopping_lists (
   name TEXT NOT NULL,
   active BOOLEAN DEFAULT TRUE
 );
+
 CREATE TABLE shopping_list_items (
   id SERIAL PRIMARY KEY,
   shopping_list_id INTEGER REFERENCES shopping_lists(id),
